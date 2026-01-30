056009069cd6edd1521b2780e9048136045ae7cd
Elliot Storey <elliotstorey@Elliots-MacBook-Air.local>
2026-01-30 22:18:35 +0000

    Remove .next build files, update gitignore

--- ui/.next/dev/server/pages/index.js
+++
@@ -1,8 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/index.js")
-R.c("server/chunks/ssr/ebb48_next_dist_f344b944._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__72fff964._.js")
-R.c("server/chunks/ssr/ebb48_5450c4e0._.js")
-R.c("server/chunks/ssr/[externals]__e6a4d965._.js")
-R.c("server/chunks/ssr/[root-of-the-server]__98b1dfb5._.js")
-R.m("[project]/Desktop/backbone-v9/ui/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/Desktop/backbone-v9/ui/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/Desktop/backbone-v9/ui/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/Desktop/backbone-v9/ui/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)")
-module.exports=R.m("[project]/Desktop/backbone-v9/ui/node_modules/next/dist/esm/build/templates/pages.js { INNER_PAGE => \"[project]/Desktop/backbone-v9/ui/pages/index.js [ssr] (ecmascript)\", INNER_DOCUMENT => \"[project]/Desktop/backbone-v9/ui/node_modules/next/document.js [ssr] (ecmascript)\", INNER_APP => \"[project]/Desktop/backbone-v9/ui/pages/_app.js [ssr] (ecmascript)\" } [ssr] (ecmascript)").exports
--- ui/.next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"HmLnOhfDhOS6iYVL7LgYEfEJc7ixGfuZ69kweDVXrrI=\"\n}"
